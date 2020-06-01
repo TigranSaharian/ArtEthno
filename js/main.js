4719bc87d8b3d025333b38612dbe9837bafe7d73
Tigran <tigran.saharyan@gmail.com>
2020-06-01 18:53:11 +0400

    about us and footer redesign

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-
 $(function(){
+    $(".sub-categoria").click(function(){
+        let firstFilter = $(".drop-menu-second-filter");
+        let section = firstFilter.closest('.row').find('section');
+        section.css('border-left', '0.5px solid #F5CD79');
+        section.css('display', 'block');
+    });
+    
+    $(document).on('click', '.material-name', function(){
+        $(this).toggleClass("active_onclick_dark");
+    });
+    
     $(".categoria_name").click(function(){
         $(".drop-menu-contaner").slideToggle();
     });
-
-
-    // Dropdown script
     $(".dropbtn").click(function(){
         let dropdown_content = $(this).closest(".dropdown").find(".dropdown-content");
         let dropdown = $(this).find('span');
@@ -16,16 +23,5 @@ $(function(){
             dropdown.text($(this).text());
             dropdown_content.slideUp(200);
         })
-    });
-
-    $(".sub-categoria").click(function(){
-        let firstFilter = $(".drop-menu-second-filter");
-        let section = firstFilter.closest('.row').find('section');
-        section.css('border-left', '0.5px solid #F5CD79');
-        section.css('display', 'block');
-    });
-
-    $(document).on('click', '.material-name', function(){
-        $(this).toggleClass("active_onclick_dark");
     });
 });
